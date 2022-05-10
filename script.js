e35513995c1394b32502e1fb62778ef34cc4b655
Sloan-Crawford <sloan.crawford@gmail.com>
2022-05-10 14:13:08 -0400

    added modal functionality - start

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Select Elements:
+const open = document.getElementById('open');
+const modalContainer = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
 // Sticky navbar on scroll
 $(document).ready(function() {
  $(window).scroll(function() {
@@ -16,3 +21,11 @@ $(document).ready(function() {
   $('.menu-btn').toggleClass("rotate");
  })
 })
+
+open.addEventListener('click', () => {
+ modalContainer.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+ modalContainer.classList.remove('show');
+});
